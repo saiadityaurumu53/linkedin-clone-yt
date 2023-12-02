fb56dcf0a43c4bcfab48361ed9e54e3f6ae5263e
Sai Aditya Urumu <saiadityaurumu53@gmail.com>
2023-12-02 09:01:04 -0800

    created a new branch with name newBranch1, installed material UI and working on the Header section

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+    <div className='header'>
+      <h1>This is a header</h1>
+
+      <div className='header__left'>
+        
+            <img src='' alt='' />
+
+            <div className='header__search'>
+                <SearchIcon/>
+                <input type='text' />
+
+            </div>
+
+      </div>
+
+      <div className='header__right'>
+
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Header
