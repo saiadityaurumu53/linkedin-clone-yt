2d01bdb1d15b1f02bb3e77558ecb00dba48da011
Sai Aditya Urumu <saiadityaurumu53@gmail.com>
2023-12-05 00:33:42 -0800

    Added the InputOptions for the Feed section

--- src/Feed.js
+++ src/Feed.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import "./Feed.css";
+import InputOption from './InputOption'; 
 
 import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+
 
 
 function Feed() {
@@ -20,14 +26,20 @@ function Feed() {
             </div>
 
             <div className='feed__inputOptions'>
+                {/* */}
+
+                <InputOption Icon={ImageIcon} title='Photo' color="#70B5F9" />
+                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E" />
+                <InputOption Icon={EventNoteIcon} title='Event' color="C0CBCD" />
+                <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#FC15E" />
+
 
                 
 
             </div>
 
 
-        </div>
-      
+        </div>    
     </div>
     );
 }
